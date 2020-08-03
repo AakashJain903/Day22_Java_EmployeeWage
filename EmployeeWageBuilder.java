a36b720e1f46efb05249aa20f9731e495a286fbb
AakashJain903 <aakash.jain903@gmail.com>
2020-08-03 12:50:33 +0530

    Added : Check Employee Present or absent

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,7 +1,27 @@
+interface EmployeePresent
+{
+	 void empChk();
+}
+
+class empCheck implements EmployeePresent
+{
+	static final int isFullTime=1;
+	double empCheck = Math.floor(Math.random()*10)%2;
+	public void empChk()
+	{
+		if (empCheck == isFullTime)
+			System.out.println("Employee is Present");
+
+		else
+			System.out.println("Employee is Absent");		
+	}
+}
+
 class EmployeeWageBuilder
 {
 	public static void main(String arg[])
 	{
-		System.out.println("Welcome to employee wage computation");
+		empCheck rm = new empCheck();
+		rm.empChk();
 	}
 }
