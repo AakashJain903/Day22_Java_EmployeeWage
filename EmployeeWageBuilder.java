b8b60fc5c8247c06f4fbed2a4b5bee7c138df3e1
AakashJain903 <aakash.jain903@gmail.com>
2020-08-03 13:16:09 +0530

    Added : Part time Employee wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,22 +1,28 @@
-interface EmpDailyWage
+interface PartTimeEmp
 {
 	 void dailyWage();
 }
 
-class wageCal implements EmpDailyWage
+class wageCal implements PartTimeEmp
 {
 	static final int isFullTime=1;
+	static final int isPartTime=2;
 	static final int empRatePerHrs=20;
 	int empHrs=0;
 	int empWage=0;
 
-	double empCheck = Math.floor(Math.random()*10)%2;
+	double empCheck = Math.floor(Math.random()*10)%3;
 	
 	public void dailyWage()
 	{
 		if (empCheck == isFullTime)
 		{
-			System.out.println("Employee is Present");
+			System.out.println("Employee is full time");
+			empHrs=12;
+		}
+		else if (empCheck == isPartTime) 
+		{
+			System.out.println("Employee is part time");
 			empHrs=8;
 		}
 		else
