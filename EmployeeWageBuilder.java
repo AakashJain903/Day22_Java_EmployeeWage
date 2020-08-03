06080750e2681f1e4ab8d3da286eaec96d5b50c8
AakashJain903 <aakash.jain903@gmail.com>
2020-08-03 13:06:36 +0530

    Added : Employee's Daily wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,19 +1,32 @@
-interface EmployeePresent
+interface EmpDailyWage
 {
-	 void empChk();
+	 void dailyWage();
 }
 
-class empCheck implements EmployeePresent
+class wageCal implements EmpDailyWage
 {
 	static final int isFullTime=1;
+	static final int empRatePerHrs=20;
+	int empHrs=0;
+	int empWage=0;
+
 	double empCheck = Math.floor(Math.random()*10)%2;
-	public void empChk()
+	
+	public void dailyWage()
 	{
 		if (empCheck == isFullTime)
+		{
 			System.out.println("Employee is Present");
-
+			empHrs=8;
+		}
 		else
+		{
 			System.out.println("Employee is Absent");		
+			empHrs=0;
+		}
+
+		empWage = empHrs * empRatePerHrs;
+		System.out.println("Employee's Daily Wage is :" + empWage);
 	}
 }
 
@@ -21,7 +34,7 @@ class EmployeeWageBuilder
 {
 	public static void main(String arg[])
 	{
-		empCheck rm = new empCheck();
-		rm.empChk();
+		wageCal wage = new wageCal();
+		wage.dailyWage();
 	}
 }
