c8ebcd2f02d358c63f858de604a630c40bad0c01
AakashJain903 <aakash.jain903@gmail.com>
2020-08-03 13:30:32 +0530

    Added : Employee's Month Wages

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,40 +1,46 @@
-interface EmployeeWage
+interface EmployeeMonthWage
 {
 	 void dailyWage();
 }
 
-class wageCal implements EmployeeWage
+class wageCal implements EmployeeMonthWage
 {
 	public static final int isFullTime=1;
 	public static final int isPartTime=2;
 	public static final int empRatePerHrs=20;
+	public static final int numOfWorkingDays=20;
 	int empHrs=0;
 	int empWage=0;
+	int totalWage=0;
 
-	int empCheck = (int) Math.floor(Math.random()*10)%3;
-	
 	public void dailyWage()
 	{
-		switch (empCheck)
+		for (int day=1; day<=numOfWorkingDays; day++)
 		{
-			case isFullTime :
-				System.out.println("Employee is full time working");
-				empHrs = 8;
-				break;
+			int empCheck = (int) Math.floor(Math.random()*10) % 3;
+			switch (empCheck)
+			{
+				case isFullTime :
+					System.out.println("Employee is full time working");
+					empHrs = 8;
+					break;
 
-			case isPartTime :
-				System.out.println("Employee is part time working");
-				empHrs = 4;
-				break;
+				case isPartTime :
+					System.out.println("Employee is part time working");
+					empHrs = 4;
+					break;
 
-			default :
-				System.out.println("Employee is Absent");
-				empHrs = 0;
-		}
+				default :
+					System.out.println("Employee is Absent");
+					empHrs = 0;
+			}
 
-		empWage = empHrs * empRatePerHrs;
-		System.out.println("Employee's Daily Wage is :" + empWage);
-	}
+			empWage = empHrs * empRatePerHrs ;
+			totalWage += empWage;
+			System.out.println("Employee's day "+ day +" Wage is : " + empWage);
+		}		
+   		System.out.println("Total Monthly wages is :" + totalWage);
+   	}	
 }
 
 class EmployeeWageBuilder
